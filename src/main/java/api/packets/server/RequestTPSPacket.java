da4bf923e1137a6fd8dbbb5a3272aa128e597372
SkyBeastMC <skybeastmc@gmail.com>
2016-12-20 19:46:38 +0100

    Server state

--- src/main/java/api/packets/server/RequestTPSPacket.java
+++ src/main/java/api/packets/server/RequestTPSPacket.java
@@ -1,56 +1,10 @@
 package api.packets.server;
 
-import api.packets.Callback;
-import api.packets.MessengerClient;
 import api.packets.OutPacket;
-import api.utils.Wrapper;
-
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by loucass003 on 07/12/16.
  */
 public class RequestTPSPacket extends OutPacket
 {
-	private static final RequestTPSPacket CACHE = new RequestTPSPacket(); //Cache because no needs to re-instantiate
-	// this class
-	private static final Lock LOCK = new ReentrantLock();
-
-	public static void request(MessengerClient client, Callback<byte[]> callback) throws IOException
-	{
-		short transaction = client.sendPacket(CACHE);
-
-		client.listenPacket(SendTPSPacket.class, transaction, tps -> callback.response(tps.getLastTPS()));
-	}
-
-	public static byte[] request(MessengerClient client) throws IOException, InterruptedException
-	{
-		return request(client, 0, null);
-	}
-
-	public static byte[] request(MessengerClient client, long timeout, TimeUnit unit) throws IOException,
-			InterruptedException
-	{
-		short transaction = client.sendPacket(CACHE);
-		Condition condition = LOCK.newCondition();
-		Wrapper<SendTPSPacket> out = new Wrapper<>();
-
-		client.listenPacket(SendTPSPacket.class, transaction, sent ->
-				{
-					out.set(sent);
-					condition.signalAll();
-				}
-		);
-
-		if (timeout == 0) //Wait until the packet arrived
-			condition.await();
-		else
-			condition.await(timeout, unit);
-
-		return out.get().getLastTPS();
-	}
 }
---
+++ src/main/java/api/packets/server/ServerStatePacket.java
@@ -0,0 +1,98 @@
+package api.packets.server;
+
+import api.packets.IncPacket;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+/**
+ * Created by SkyBeast on 20/12/2016.
+ */
+public class ServerStatePacket extends IncPacket
+{
+	private final ServerState serverState;
+
+	public ServerStatePacket(DataInputStream data) throws IOException
+	{
+		super(data);
+		this.serverState = ServerState.values()[data.readInt()];
+	}
+
+	public ServerState getServerState()
+	{
+		return serverState;
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		ServerStatePacket that = (ServerStatePacket) o;
+
+		return serverState == that.serverState;
+
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return serverState != null ? serverState.hashCode() : 0;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "ServerStatePacket{" +
+				"serverState=" + serverState +
+				'}';
+	}
+
+	public enum ServerState
+	{
+
+		/**
+		 * The server is started
+		 */
+		STARTED,
+
+		/**
+		 * The server is ready to accept players
+		 */
+		READY,
+
+		/**
+		 * NON-LOBBY
+		 * The game is waiting for players
+		 */
+		WAITING_PLAYERS,
+
+		/**
+		 * NON-LOBBY
+		 * The game is started
+		 */
+		GAME_STARTED,
+
+		/**
+		 * NON-LOBBY
+		 * The game is ended but the server is not stopping yet
+		 */
+		GAME_ENDED,
+
+		/**
+		 * The server is stopping because of an error
+		 */
+		STOP_ERROR,
+
+		/**
+		 * The server is auto-stopping
+		 */
+		STOPPING,
+
+		/**
+		 * The server is stopping because we told him to do
+		 */
+		STOPPING_ASKED
+	}
+}
